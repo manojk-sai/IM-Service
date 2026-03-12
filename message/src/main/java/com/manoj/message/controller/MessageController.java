a7e17e4e41f4988c639f41c26ab16fb43459e57a
manojk-sai <manojsaikothapati@gmail.com>
2026-03-12 12:43:29 -0500

    Message-service

--- message/src/main/java/com/manoj/message/controller/MessageController.java
+++ message/src/main/java/com/manoj/message/controller/MessageController.java
@@ -1,4 +1,26 @@
 package com.manoj.message.controller;
 
+import com.manoj.message.model.Message;
+import com.manoj.message.service.MessageService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/messages")
 public class MessageController {
+
+    private final MessageService messageService;
+
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+     }
+
+    @GetMapping("/{chatId}")
+    public List<Message> getMessages(@PathVariable String chatId) {
+        return messageService.getChatHistory(chatId);
+    }
 }
