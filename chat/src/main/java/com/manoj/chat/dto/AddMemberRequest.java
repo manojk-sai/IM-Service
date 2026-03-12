94ad4100cab9ac68047045b8f2e50526f648a024
manojk-sai <manojsaikothapati@gmail.com>
2026-03-12 13:17:04 -0500

    Bug: Input JSON Validations

--- chat/src/main/java/com/manoj/chat/dto/AddMemberRequest.java
+++ chat/src/main/java/com/manoj/chat/dto/AddMemberRequest.java
@@ -1,10 +1,11 @@
 package com.manoj.chat.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AddMemberRequest {
-
+    @NotBlank(message = "Username is required")
     private String username;
 
 }
--- chat/src/main/java/com/manoj/chat/dto/CreateChatRequest.java
+++ chat/src/main/java/com/manoj/chat/dto/CreateChatRequest.java
@@ -1,16 +1,21 @@
 package com.manoj.chat.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class CreateChatRequest {
-
+    @NotBlank(message = "Chat name is required")
     private String name;
-
+    @NotBlank(message = "Chat type is required")
+    @Pattern(regexp = "PRIVATE|GROUP", message = "Chat type must be either PRIVATE or GROUP")
     private String type;
-
+    @NotNull(message = "Members list is required")
+    @Size(min = 1, message = "At least one member is required")
     private List<String> members;
-
 }
